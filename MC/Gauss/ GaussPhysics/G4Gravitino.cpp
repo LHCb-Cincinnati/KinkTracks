e9b8313d1ae348164a7f01c96ca2aabf4453bc9a
Mohamed Elashri <muhammadelashri@gmail.com>
2022-10-01 18:46:18 -0400

    Add correct src files that will build on nightly and lb-dev.

--- MC/Gauss/ GaussPhysics/G4Gravitino.cpp
+++ MC/Gauss/ GaussPhysics/G4Gravitino.cpp
@@ -1,8 +1,19 @@
+/*****************************************************************************\
+* (c) Copyright 2000-2020 CERN for the benefit of the LHCb Collaboration      *
+*                                                                             *
+* This software is distributed under the terms of the GNU General Public      *
+* Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   *
+*                                                                             *
+* In applying this licence, CERN does not waive the privileges and immunities *
+* granted to it by virtue of its status as an Intergovernmental Organization  *
+* or submit itself to any jurisdiction.                                       *
+\*****************************************************************************/
+
 #include <fstream>
 #include <iomanip>
 
 #include "G4Gravitino.h"
-#include "Geant4/G4ParticleTable.hh"
+#include "G4ParticleTable.hh"
 
 // ###################################################################### 
 // ###                       Gravitino                                ###
@@ -43,4 +54,3 @@ G4Gravitino * G4Gravitino:: Definition()
 
 G4Gravitino*  G4Gravitino::GravitinoDefinition(){return Definition();}
 G4Gravitino*  G4Gravitino::Gravitino(){return Definition();}
-
--- MC/Gauss/ GaussPhysics/G4Gravitino.h
+++ MC/Gauss/ GaussPhysics/G4Gravitino.h
@@ -1,10 +1,20 @@
+/*****************************************************************************\
+* (c) Copyright 2000-2020 CERN for the benefit of the LHCb Collaboration      *
+*                                                                             *
+* This software is distributed under the terms of the GNU General Public      *
+* Licence version 3 (GPL Version 3), copied verbatim in the file "COPYING".   *
+*                                                                             *
+* In applying this licence, CERN does not waive the privileges and immunities *
+* granted to it by virtue of its status as an Intergovernmental Organization  *
+* or submit itself to any jurisdiction.                                       *
+\*****************************************************************************/
 
 #ifndef G4Gravitino_h 
 #define G4Gravitino_h 1 
 
-#include "Geant4/globals.hh"
-#include "Geant4/G4ios.hh"
-#include "Geant4/G4ParticleDefinition.hh"
+#include "globals.hh"
+#include "G4ios.hh"
+#include "G4ParticleDefinition.hh"
 
 /** @class  G4Gravitino G4Gravitino.h  *
  * 
