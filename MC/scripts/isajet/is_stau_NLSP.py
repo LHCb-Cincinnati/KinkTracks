b42f65e5b2acfcfd2af322c2b8f1d4db57e000b8
Mohamed Elashri <mail@elashri.com>
2023-04-09 00:04:36 -0400

    Fix issues with the determination logic
    1. We ignore negative masses that might occur.
    2. Fix an error that occurs because `pdg_id` is being read as a string instead of an integer.

--- MC/scripts/isajet/is_stau_NLSP.py
+++ MC/scripts/isajet/is_stau_NLSP.py
@@ -7,28 +7,27 @@ spc = xslha.read(slha_file)
 # Extract particle masses
 masses = spc.blocks['MASS']
 
-# Filter out superpartners and gravitino
-superpartners = {pdg_id: mass for pdg_id, mass in masses.items() if abs(pdg_id) > 1000000}
+# Filter out superpartners, gravitino, and negative masses
+superpartners = {int(pdg_id): mass for pdg_id, mass in masses.items() if abs(int(pdg_id)) > 1000000 and mass > 0}
 gravitino_mass = superpartners.get(1000039, None)
 
-# Check if gravitino is the LSP
-if gravitino_mass is not None:
-    lsp_pdg_id = min(superpartners, key=superpartners.get)
-    lsp_mass = superpartners[lsp_pdg_id]
+# Determine the LSP
+lsp_pdg_id = min(superpartners, key=superpartners.get)
+lsp_mass = superpartners[lsp_pdg_id]
 
-    if lsp_pdg_id == 1000039:
-        print("Gravitino is the LSP.")
-
-        # Check if stau is the NLSP
-        del superpartners[1000039]
-        nlsp_pdg_id = min(superpartners, key=superpartners.get)
-        nlsp_mass = superpartners[nlsp_pdg_id]
-
-        if nlsp_pdg_id in [1000015, 2000015]:
-            print("Stau is the NLSP.")
-        else:
-            print(f"The NLSP is not stau. PDG ID of the NLSP: {nlsp_pdg_id}")
-    else:
-        print("Gravitino is not the LSP.")
+if lsp_pdg_id == 1000039:
+    print("Gravitino is the LSP.")
 else:
-    print("Gravitino not found in the SLHA file.")
+    print(f"Gravitino is not the LSP. The LSP is a particle with PDG ID: {lsp_pdg_id} and mass: {lsp_mass}")
+
+# Check if stau is the NLSP
+if gravitino_mass is not None:
+    del superpartners[1000039]
+
+nlsp_pdg_id = min(superpartners, key=superpartners.get)
+nlsp_mass = superpartners[nlsp_pdg_id]
+
+if nlsp_pdg_id in [1000015, 2000015]:
+    print("Stau is the NLSP.")
+else:
+    print(f"The NLSP is not stau. PDG ID of the NLSP: {nlsp_pdg_id}")
