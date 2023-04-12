18beb00bd66b76baa2fe3394705d057a619ce022
Mohamed <muhammadelashri@gmail.com>
2023-04-12 00:32:24 -0400

    update plots

--- MC/pythia/plotting/len_fit_all.py
+++ MC/pythia/plotting/len_fit_all.py
@@ -18,17 +18,29 @@ for ln in ln_values:
         pp = PdfPages(f'DecayLength_{ms}GeV_{ln}mm_fit.pdf')
         tmp1 = plt.figure(1)
         tmp1.set_size_inches(8.00, 6.00)
-        plot = open(f'../DecayLength_{ms}GeV_{ln}mm-0.dat')
+        plot = open(f'data/decay_length_data/DecayLength_{ms}GeV_{ln}mm-0.dat')
         plot = [line.split() for line in plot]
         valx = [float(x[0]) for x in plot]
         valy = [float(x[1]) for x in plot]
         vale = [float(x[2]) for x in plot]
-        plt.hist(valx, vale, weights=valy, histtype='step', color='red', label=r'Stau ({ms}GeV), ctau = {ln} mm')
+        plt.hist(valx, vale, weights=valy, histtype='step', color='red', label=fr'Stau ({ms} GeV), ctau = {ln} mm')
 
         # Fit the data
         popt, pcov = curve_fit(exp_func, valx, valy, p0=[1, 1e-4, 1], maxfev=10000)
         fit_y = exp_func(np.array(valx), *popt)
         plt.plot(valx, fit_y, label=f'Fit: a={popt[0]:.2e}, b={popt[1]:.2e}, c={popt[2]:.2e}')
+        
+        # Calculate and display lifetime
+        lifetime = 1 / popt[1]
+        # convert lifetime from mm to m
+        lifetime = lifetime / 1000.0
+        plt.text(0.55, 0.85, f'Lifetime: {lifetime:.2e}' ' m',
+                 transform=plt.gca().transAxes,
+                 fontsize=10,
+                 verticalalignment='top',
+                 bbox=dict(facecolor='white', alpha=0.8),
+                 fontweight='bold')
+
 
         plt.xlim(0.000e+00, 1.000e+04)
         plt.ticklabel_format(axis='y', style='sci', scilimits=(-2, 3))
--- MC/pythia/plotting/len_impose_plot.py
+++ MC/pythia/plotting/len_impose_plot.py
@@ -3,7 +3,7 @@ from matplotlib.backends.backend_pdf import PdfPages
 import os
 
 
-ln = '10000'
+ln = '10'
 
 # Read data from the files
 def read_data(filename):
@@ -14,16 +14,16 @@ def read_data(filename):
     vale = [float(x[2]) for x in data]
     return valx, valy, vale
 
-valx_100, valy_100, vale_100 = read_data('../DecayLength_100GeV_'+ln+'mm-0.dat')
-valx_150, valy_150, vale_150 = read_data('../DecayLength_150GeV_'+ln+'mm-0.dat')
-valx_200, valy_200, vale_200 = read_data('../DecayLength_200GeV_'+ln+'mm-0.dat')
-valx_250, valy_250, vale_250 = read_data('../DecayLength_250GeV_'+ln+'mm-0.dat')
-valx_300, valy_300, vale_300 = read_data('../DecayLength_300GeV_'+ln+'mm-0.dat')
+valx_100, valy_100, vale_100 = read_data('data/decay_length_data/DecayLength_100GeV_'+ln+'mm-0.dat')
+valx_150, valy_150, vale_150 = read_data('data/decay_length_data/DecayLength_150GeV_'+ln+'mm-0.dat')
+valx_200, valy_200, vale_200 = read_data('data/decay_length_data/DecayLength_200GeV_'+ln+'mm-0.dat')
+valx_250, valy_250, vale_250 = read_data('data/decay_length_data/DecayLength_250GeV_'+ln+'mm-0.dat')
+valx_300, valy_300, vale_300 = read_data('data/decay_length_data/DecayLength_300GeV_'+ln+'mm-0.dat')
 
 # Create the plot
 pp = PdfPages('DecayLength_combined_' + ln + 'mm.pdf')
 tmp1 = plt.figure(1)
-tmp1.set_size_inches(25.00, 15.00)
+tmp1.set_size_inches(10.00, 6.00)
 
 plt.hist(valx_100, vale_100, weights=valy_100, histtype='step', color='red', label=r'Stau (100 GeV), ctau =' + ln + ' mm')
 plt.hist(valx_150, vale_150, weights=valy_150, histtype='step', color='blue', label=r'Stau (150 GeV), ctau =' + ln + ' mm')
@@ -32,8 +32,8 @@ plt.hist(valx_250, vale_250, weights=valy_250, histtype='step', color='orange', 
 plt.hist(valx_300, vale_300, weights=valy_300, histtype='step', color='purple', label=r'Stau (300 GeV), ctau =' + ln + ' mm')
 
 
-plt.xlim(0.000e+00, 1.000e+04)
-plt.ylim(0.000e+00, max(max(valy_100), max(valy_300)) * 1.1)  
+plt.xlim(0.000e+00, 2.000e+03)
+#plt.ylim(0.000e+00, max(max(valy_100), max(valy_300)) * 1.1)  
 
 plt.ticklabel_format(axis='y', style='sci', scilimits=(-2, 3))
 plt.legend(frameon=False, loc='best')
