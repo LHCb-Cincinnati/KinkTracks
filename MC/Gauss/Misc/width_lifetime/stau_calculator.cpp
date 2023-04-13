e3398f3d3325e15f8729186e3e68e90ed2e339b2
Mohamed <muhammadelashri@gmail.com>
2023-04-13 09:31:22 -0400

    Fix th main function

--- MC/Gauss/Misc/width_lifetime/stau_calculator.cpp
+++ MC/Gauss/Misc/width_lifetime/stau_calculator.cpp
@@ -17,54 +17,51 @@ const double s = 1.0;                      // s, 1 second in s
 const double ns = 1.0e-9;                  // s, 1 nanosecond in s
 
 void calculateWidthAndLifetime(double inputWidth, double inputLifetime_ns, double& outputWidth, double& outputLifetime_ns) {
-    // Convert input lifetime from ns to s
-    double inputLifetime = inputLifetime_ns * ns;
+    double inputLifetime = inputLifetime_ns * ns; // Convert input lifetime from ns to s
 
-    // Check if width is given
     if (inputWidth > 0) {
         outputWidth = inputWidth;
-        // Convert output lifetime to ns
-        outputLifetime_ns = hbar_Planck / inputWidth / ns;
-    // Check if lifetime is given
+        outputLifetime_ns = hbar_Planck / inputWidth / ns; // Convert output lifetime to ns
     } else if (inputLifetime > 0) {
         outputLifetime_ns = inputLifetime_ns;
         outputWidth = hbar_Planck / inputLifetime;
-    // Neither width nor lifetime is given
     } else {
         outputWidth = -1;
         outputLifetime_ns = -1;
     }
 }
 
-// Main function
-    // Start with a string of input lifetime.
-    // The input lifetime is a string from the command line.
-    // This string must be converted to a long integer.
-    // The long integer is the input lifetime in nanoseconds.
-    long input_lifetime_ns = 0;
-    try {
-        input_lifetime_ns = std::stol(argv[1]);
-    } catch (...) {
-        std::cerr << "Error: invalid input lifetime: " << argv[1] << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <input_lifetime_ns>" << std::endl;
         return 1;
     }
 
-    // Check if the input lifetime is negative.
+    // Main function
+    long input_lifetime_ns = 0;
+    try {
+        input_lifetime_ns = std::stol(argv[1]);
+    } catch (const std::invalid_argument& e) {
+        std::cerr << "Error: invalid input lifetime: " << argv[1] << std::endl;
+        return 1;
+    } catch (const std::out_of_range& e) {
+        std::cerr << "Error: input lifetime out of range: " << argv[1] << std::endl;
+        return 1;
+    }
+
     if (input_lifetime_ns < 0) {
         std::cerr << "Error: negative input lifetime: " << argv[1] << std::endl;
         return 1;
     }
 
-    // Check if the input lifetime is zero.
-    // Warn the user that this might not be what they intended.
     if (input_lifetime_ns == 0) {
         std::cerr << "Warning: zero input lifetime: " << argv[1] << std::endl;
     }
+
     double inputWidth = -1;
-    double inputLifetime_ns = std::atof(argv[1]); // Parse input lifetime value in ns from command-line argument
     double outputWidth, outputLifetime_ns;
 
-    calculateWidthAndLifetime(inputWidth, inputLifetime_ns, outputWidth, outputLifetime_ns);
+    calculateWidthAndLifetime(inputWidth, static_cast<double>(input_lifetime_ns), outputWidth, outputLifetime_ns);
 
     std::cout << "Stau width in MeV: " << outputWidth / MeV << " MeV" << std::endl;
     std::cout << "Stau width in GeV: " << outputWidth / GeV << " GeV" << std::endl;
